9ba7a778c33a929e7e312f427e9016257ebdae93
mirza <2022453238@student.uitm.edu.my>
2025-04-28 21:57:14 +0800

    Remove bottom scrollbar

--- frontend/src/components/map.tsx
+++ frontend/src/components/map.tsx
@@ -281,7 +281,7 @@ export default function Map({
 
       {/* Location cards at the bottom */}
       <div className="absolute bottom-4 left-0 right-0 z-10 px-4">
-        <div className="flex overflow-x-auto gap-3 pb-1">
+        <div className="flex overflow-x-auto gap-3 pb-1 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
           {locations.map((location) => (
             <div
               key={location.id}
